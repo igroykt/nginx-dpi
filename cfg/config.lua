dc38e7777b25dfd70cc169d8d64f0f257fa33701
igroykt <artofmotion.igroman@gmail.com>
2024-08-08 17:01:33 +0500

    async port_test; http_v3 support;

---
+++ cfg/config.lua
@@ -0,0 +1,15 @@
+return {
+	proxy_addr = "127.0.0.1",
+	proxy_port = 1082,
+	isp_block_banner = "fz139.ttk.ru",
+	isp_common_name = "ttk.ru",
+	exclude_domains = {
+		".ru",
+		"igro.tech"
+	},
+	force_domains = {
+	},
+	connection_timeout = 2000,
+	read_timeout = 2000,
+	write_timeout = 2000
+}
--- cfg/start.sh
+++ cfg/start.sh
@@ -19,15 +19,10 @@ RAM_SIZE=`free -gt|grep Mem|awk '{print $2}'`
 CONNTRACK_MAX=`echo "$RAM_SIZE*1024^3/16384/2"|bc`
 sysctl -w net.netfilter.nf_conntrack_max=$CONNTRACK_MAX > /dev/null
 
-# redirect
+# redirect http
 iptables -t nat -A PREROUTING -i $INTERFACE -p tcp -m tcp --dport 80 \! -d $NETWORK -j REDIRECT --to-ports 30443
 iptables -t nat -A PREROUTING -i $INTERFACE -p tcp -m tcp --dport 443 \! -d $NETWORK -j REDIRECT --to-ports 30443
 
-# igmp
-sysctl -w net.ipv4.conf.all.force_igmp_version=2 > /dev/null
-sysctl -w net.ipv4.igmp_max_memberships=20 > /dev/null
-sysctl -w net.ipv4.igmp_max_msf=10 > /dev/null
-
-# iptv
-iptables -t nat -A PREROUTING -m addrtype --dst-type MULTICAST --limit-iface-in -j ACCEPT
-iptables -t nat -A PREROUTING -s $NETWORK -p udp -m udp --dport 1234 -j ACCEPT
+# redirect http_v3
+iptables -t nat -A PREROUTING -i $INTERFACE -p udp -m udp --dport 80 \! -d $NETWORK -j REDIRECT --to-ports 30443
+iptables -t nat -A PREROUTING -i $INTERFACE -p udp -m udp --dport 443 \! -d $NETWORK -j REDIRECT --to-ports 30443
